d0dae3ee3cb59948d13848a1ec71e518d52d4bb9
Anjola <anjola.aina@gmail.com>
2022-01-13 22:34:32 -0700

    Created what the card should look like for the prototype

--- app/src/main/java/com/example/foodpantry/Fragment1.java
+++ app/src/main/java/com/example/foodpantry/Fragment1.java
@@ -23,13 +23,6 @@ public class Fragment1 extends Fragment {
                            Bundle savedInstanceState) {
     // Inflate the layout for this fragment
     View fragment1View =  inflater.inflate(R.layout.fragment_1, container, false);
-    Button aButton = fragment1View.findViewById(R.id.button);
-    aButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Navigation.findNavController(fragment1View).navigate(R.id.action_fragment1_to_scrollingFragment2);
-      }
-    });
     return fragment1View;
   }
 } // class
---
+++ app/src/main/java/com/example/foodpantry/NavigationBarFragment.java
@@ -0,0 +1,6 @@
+package com.example.foodpantry;
+
+import android.app.Fragment;
+
+public class NavigationBarFragment extends Fragment {
+}
