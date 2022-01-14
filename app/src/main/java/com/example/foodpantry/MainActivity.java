5d5b7739c7b897dcccb31dc2de3bd13c8a9aa4f1
Anjola <anjola.aina@gmail.com>
2022-01-14 13:37:02 -0700

    Completely reworked and finished designing the pantry fragment (for now)

--- app/src/main/java/com/example/foodpantry/MainActivity.java
+++ app/src/main/java/com/example/foodpantry/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.foodpantry;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-  }
+    // When the user opens the app, the keyboard doesn't appear automatically
+    getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+  } // onCreate
 
 } // class
