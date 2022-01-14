0be8c86db1066b48e5228f92670a78fdade5bf9a
Harry <stefanharry2@gmail.com>
2022-01-14 16:05:50 -0700

    Literally changed nothing, android studio is weird

--- app/src/main/java/com/example/foodpantry/MainActivity.java
+++ app/src/main/java/com/example/foodpantry/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.foodpantry;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+  Toast lastToast;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  public void showToast(String text){
+    if(lastToast != null){
+      lastToast.cancel();
+    }
+    Toast toast = Toast.makeText(this, text, Toast.LENGTH_LONG);
+    toast.setGravity(Gravity.BOTTOM, 0, 0);
+    toast.show();
+    lastToast = toast;
   }
 
 } // class
---
+++ app/src/main/java/com/example/foodpantry/PantryItem.java
@@ -0,0 +1,15 @@
+package com.example.foodpantry;
+
+import java.util.Date;
+
+public class PantryItem {
+
+  String foodName;
+  Date expiryDate;
+  int foodCategory;
+  int amount;
+
+  public String getFoodName(String name){
+    return foodName;
+  }
+}
