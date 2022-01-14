a37f54888229c86b1ef9e4adefc6918238976e23
Anjola <anjola.aina@gmail.com>
2022-01-14 16:50:02 -0700

    Finished creating all of the prototype XML files

--- app/src/main/java/com/example/foodpantry/ExpiringSoonFragment.java
+++ app/src/main/java/com/example/foodpantry/ExpiringSoonFragment.java
@@ -3,34 +3,23 @@ package com.example.foodpantry;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 /**
- * Used to navigate through other fragments, I guess.
+ *
  */
-public class NavigationBarFragment extends Fragment {
+public class ExpiringSoonFragment extends Fragment {
 
-
-  public NavigationBarFragment() {
+  public ExpiringSoonFragment() {
     // Required empty public constructor
   }
-
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
-    View navigationBarView = inflater.inflate(R.layout.fragment_navigation_bar, container, false);
     // Inflate the layout for this fragment
-    return navigationBarView;
-  } // onCreateView
-
-
-
-
-
-
+    return inflater.inflate(R.layout.fragment_expiring_soon, container, false);
+  }
 } // class
