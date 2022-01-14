90f9492a2cf86764f9d04429fd5f8dbbe2f39bf5
Anjola <anjola.aina@gmail.com>
2022-01-14 12:34:17 -0700

    Fixed the pantry fragment layout

---
+++ app/src/main/java/com/example/foodpantry/LowInStockFragment.java
@@ -0,0 +1,27 @@
+package com.example.foodpantry;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LowInStockFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LowInStockFragment extends Fragment {
+
+  public LowInStockFragment() {
+    // Required empty public constructor
+  }
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_low_in_stock, container, false);
+  }
+}
