c7b703374570063d8d393aadd60c0357ea3dfdf0
Harry <stefanharry2@gmail.com>
2022-01-14 17:21:42 -0700

    Merged Anjola's branch

--- app/src/main/java/com/example/foodpantry/PantryItem.java
+++ app/src/main/java/com/example/foodpantry/PantryItem.java
@@ -4,12 +4,47 @@ import java.util.Date;
 
 public class PantryItem {
 
-  String foodName;
+  String itemName;
   Date expiryDate;
   int foodCategory;
   int amount;
 
   public String getFoodName(String name){
-    return foodName;
+    return itemName;
+  }
+
+  public void Item(String itemName, Date expDate, int amount){
+    this.itemName = itemName;
+    this.expiryDate = expDate;
+    this.amount = amount;
+  }
+  public void Item(){
+    this.itemName = itemName;
+    this.amount = amount;
+    this.expiryDate = expiryDate;
+  }
+
+  public void setItem(String itemName){
+    this.itemName = itemName;
+  }
+
+  public void setAmount(int amount) {
+    this.amount = amount;
+  }
+
+  public void setExpDate(Date expDate) {
+    this.expiryDate = expDate;
+  }
+
+  public int getAmount() {
+    return amount;
+  }
+
+  public String getItemName() {
+    return itemName;
+  }
+
+  public Date getExpDate() {
+    return expiryDate;
   }
 }
