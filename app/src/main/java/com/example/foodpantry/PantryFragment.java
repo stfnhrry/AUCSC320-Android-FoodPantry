427d74d682474104fcd8c3d4743c8cae2cafdc6a
Anjola <anjola.aina@gmail.com>
2022-01-15 16:53:19 -0700

    Created a card view layout resource file to be used alongside a recycle view. Will be implementing a navigation view soon.

--- app/src/main/java/com/example/foodpantry/PantryFragment.java
+++ app/src/main/java/com/example/foodpantry/PantryFragment.java
@@ -7,11 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  *
  */
-public class PantryFragment extends Fragment {
+public class PantryFragment extends Fragment implements View.OnClickListener {
 
   public PantryFragment() {
   }
@@ -21,6 +23,25 @@ public class PantryFragment extends Fragment {
                            Bundle savedInstanceState) {
     // Inflate the layout for this fragment
     View pantryFragment =  inflater.inflate(R.layout.fragment_pantry, container, false);
+    ImageButton editButton = (ImageButton) pantryFragment.findViewById(R.id.editButtonForItem0);
+    ImageButton addToShoppingCartButton =  (ImageButton) pantryFragment.findViewById(R.id.addToShoppingCartButtonForItem0);
+    editButton.setOnClickListener(this);
+    addToShoppingCartButton.setOnClickListener(this);
     return pantryFragment;
-  }
+  } //onCreateView
+
+  @Override
+  public void onClick(View pantryFragmentView) {
+    switch (pantryFragmentView.getId()) {
+      case R.id.editButtonForItem0:
+        System.out.println("You clicked on the edit button!");
+        break;
+      case R.id.addToShoppingCartButtonForItem0:
+        System.out.println("You clicked on the add to shopping cart button!");
+        break;
+      default:
+        break;
+    } // switch
+  } // onClick
+
 } // class
