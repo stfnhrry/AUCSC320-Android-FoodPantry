7e90a6ae12d0d3fc736dbdf74f749597b0d06ab8
jamesrota <jrota@ualberta.ca>
2022-01-16 12:13:01 -0700

    Not many changes here, played around with fragments

--- app/src/main/java/com/example/foodpantry/MainActivity.java
+++ app/src/main/java/com/example/foodpantry/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.foodpantry;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.navigation.fragment.NavHostFragment;
+//import androidx.navigation.fragment.NavHostFragment;
 
 import android.net.Uri;
 import android.os.Bundle;
--- app/src/main/java/com/example/foodpantry/PantryFragment.java
+++ app/src/main/java/com/example/foodpantry/PantryFragment.java
@@ -14,45 +14,14 @@ import android.widget.TextView;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/**
- *
- */
+
 public class PantryFragment extends Fragment {
-    public AddItem test = new AddItem();
-  TextView title;
-  TextView expDate;
-  TextView amount;
-  ImageButton edit;
-  View view;
+    View view;
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     // Inflate the layout for this fragment
     view =  inflater.inflate(R.layout.fragment_pantry, container, false);
-    title = (TextView) view.findViewById(R.id.titleForItem0);
-    amount = (TextView) view.findViewById(R.id.amountLeftInPantryForItem0);
-    expDate = (TextView) view.findViewById(R.id.expiryDateForItem0);
-    edit = (ImageButton) view.findViewById(R.id.editButtonForItem0);
-    edit.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-          String e1 = expDate.getText().toString();
-          String e2 = title.getText().toString();
-          String e3 = amount.getText().toString();
-          Date date = new Date(e1);
-          int amnt = Integer.parseInt(e3);
-          Item item = new Item(e2, date, amnt);
-          System.out.println(item.getExpDate());
-          System.out.println(item.getItemName());
-          System.out.println(item.getAmount());
-
-
-
-
-
-
-      }
-    });
 
     return view;
   }
