070fec15519feefed46935f7a2db65a689e6cbbe
Harry <stefanharry2@gmail.com>
2022-01-15 22:42:39 -0700

    Can parse values when creating new items, fixed missing icons

--- app/src/main/java/com/example/foodpantry/ItemFragment.java
+++ app/src/main/java/com/example/foodpantry/ItemFragment.java
@@ -9,6 +9,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,12 +20,23 @@ import android.view.ViewGroup;
  */
 public class ItemFragment extends Fragment {
 
+  public ImageView icon;
+  public String title;
+  public String category;
+  public int amount;
+  public Date expiryDate;
+  public int size;
+
   private OnFragmentInteractionListener mListener;
 
   // TODO: Rename parameter arguments, choose names that match
   // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-  private static final String ARG_PARAM1 = "param1";
-  private static final String ARG_PARAM2 = "param2";
+  private static String ARG_TITLE = "title";
+  private static final String ARG_CATEGORY = "category";
+  public static String ARG_AMOUNT = "1";
+  public static Date ARG_DATE = new Date();
+  public static String ARG_SIZE = "5";
+
 
   // TODO: Rename and change types of parameters
   private String mParam1;
@@ -36,16 +50,18 @@ public class ItemFragment extends Fragment {
    * Use this factory method to create a new instance of
    * this fragment using the provided parameters.
    *
-   * @param param1 Parameter 1.
-   * @param param2 Parameter 2.
+   * @param title Parameter 1.
+   * @param category Parameter 2.
    * @return A new instance of fragment ItemFragment.
    */
   // TODO: Rename and change types and number of parameters
-  public static ItemFragment newInstance(String param1, String param2) {
+  public static ItemFragment newInstance(String title, String category, int number, int weight) {
     ItemFragment fragment = new ItemFragment();
     Bundle args = new Bundle();
-    args.putString(ARG_PARAM1, param1);
-    args.putString(ARG_PARAM2, param2);
+    args.putString(ARG_TITLE, title);
+    args.putString(ARG_CATEGORY, category);
+    args.putInt(ARG_AMOUNT, number);
+    args.putInt(ARG_SIZE, weight);
     fragment.setArguments(args);
     return fragment;
   }
@@ -54,8 +70,10 @@ public class ItemFragment extends Fragment {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     if (getArguments() != null) {
-      mParam1 = getArguments().getString(ARG_PARAM1);
-      mParam2 = getArguments().getString(ARG_PARAM2);
+      title = getArguments().getString(ARG_TITLE);
+      category = getArguments().getString(ARG_CATEGORY);
+      amount = getArguments().getInt(ARG_AMOUNT);
+      size = getArguments().getInt(ARG_SIZE);
     }
   }
 
--- app/src/main/java/com/example/foodpantry/MainActivity.java
+++ app/src/main/java/com/example/foodpantry/MainActivity.java
@@ -5,18 +5,31 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.view.WindowManager;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ItemFragment.OnFragmentInteractionListener {
 
   Button pantryFragment, addItem, removeItem, lowInStock, outOfStock, expiringSoon, expired, shoppingList;
+
+  Toast lastToast;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    ItemFragment one = ItemFragment.newInstance("Bread", "Pastry", 1, 1);
+    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+    transaction.add(R.id.linearLayout, one);
+    transaction.commit();
 
     pantryFragment = findViewById(R.id.pantryButton);
     addItem = findViewById(R.id.addItemButton);
@@ -31,7 +44,14 @@ public class MainActivity extends AppCompatActivity {
     pantryFragment.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        replaceFragment(new PantryFragment());
+        newCard();
+      }
+    });
+
+    addItem.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        addNewItem();
       }
     });
 
@@ -71,12 +91,45 @@ public class MainActivity extends AppCompatActivity {
     });
   }
 
+  public void showToast(String text){
+    if(lastToast != null){
+      lastToast.cancel();
+    }
+    Toast toast = Toast.makeText(this, text, Toast.LENGTH_LONG);
+    toast.setGravity(Gravity.BOTTOM, 0, 0);
+    toast.show();
+    lastToast = toast;
+  }
+
   private void replaceFragment(Fragment fragment) {
 
     FragmentManager fragmentManager = getSupportFragmentManager();
     FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
     fragmentTransaction.replace(R.id.FrameLayout, fragment);
     fragmentTransaction.commit();
+  }
+
+  @Override
+  public void messageFromChildFragment(Uri uri){
+    Log.i("TAG", "Received communication from child fragment");
+  }
+
+  public void newCard(){
+    ItemFragment two = ItemFragment.newInstance("NewCard", "Testing", 1, 0);
+    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+    transaction.add(R.id.linearLayout, two);
+    transaction.commit();
+    showToast("Hello there");
+  }
+
+  public void addNewItem(){
+    ItemFragment two = ItemFragment.newInstance("Ice Cream", "Sweets", 1, 1);
+    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+    transaction.add(R.id.linearLayout, two);
+    transaction.commit();
+    showToast("Hello there");
   }
 
 } // class
