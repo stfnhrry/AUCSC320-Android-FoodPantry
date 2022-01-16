dd7a068c6290eabbb72f5c7c8446651794d7d98f
Harry <stefanharry2@gmail.com>
2022-01-15 18:03:15 -0700

    Made each card a fragment, put cards in pantry fragment

--- app/src/main/java/com/example/foodpantry/MainActivity.java
+++ app/src/main/java/com/example/foodpantry/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.foodpantry;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.widget.Button;
 import android.widget.Toast;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PantryFragment.OnFragmentInteractionListener, ItemFragment.OnFragmentInteractionListener {
 
   Toast lastToast;
 
@@ -19,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
     // When the user opens the app, the keyboard doesn't appear automatically
     getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+    //Begin transaction
+    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+    ft.replace(R.id.fragmentContainerView, new PantryFragment());
+    ft.commit();
   } // onCreate
 
   public void showToast(String text){
@@ -35,6 +43,16 @@ public class MainActivity extends AppCompatActivity {
     Button myButton = new Button(this);
     myButton.setText("I am new here");
 
+  }
+
+  @Override
+  public void messageFromParentFragment(Uri uri) {
+    Log.i("TAG", "received communication from parent fragment");
+  }
+
+  @Override
+  public void messageFromChildFragment(Uri uri) {
+    Log.i("TAG", "received communication from child fragment");
   }
 
 } // class
--- app/src/main/java/com/example/foodpantry/PantryFragment.java
+++ app/src/main/java/com/example/foodpantry/PantryFragment.java
@@ -1,9 +1,12 @@
 package com.example.foodpantry;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.text.Layout;
 import android.view.LayoutInflater;
@@ -17,6 +20,8 @@ import android.widget.LinearLayout;
  */
 public class PantryFragment extends Fragment {
 
+  private OnFragmentInteractionListener mListener;
+
   public PantryFragment() {
   }
 
@@ -26,37 +31,36 @@ public class PantryFragment extends Fragment {
     // Inflate the layout for this fragment
     View pantryFragment =  inflater.inflate(R.layout.fragment_pantry, container, false);
 
-    //Button myButton1 = new Button(getActivity());
-    //myButton1.setText("Press me");
-
-    //View newButton = inflater.inflate(R.layout.item_card, container, false);
-
-    final Button myButton = new Button(pantryFragment.getContext());
-    myButton.setText("Press me please");
-
-    myButton.setBackgroundColor(getResources().getColor(R.color.orange_warning));
-    myButton.setTextSize(18);
-    myButton.setPadding(20, 0, 20, 0);
-
-    ConstraintLayout constraintLayout = (ConstraintLayout) pantryFragment.findViewById(R.id.scrollIViewForPantryFragmentConstraint);
-
-    //LinearLayout linearlayout = (LinearLayout) myView.findViewById(R.id.btnholder);
-    //linearlayout.setOrientation(LinearLayout.VERTICAL);
-
-    ConstraintLayout.LayoutParams cParams = new ConstraintLayout.LayoutParams(
-            ConstraintLayout.LayoutParams.MATCH_PARENT,
-            ConstraintLayout.LayoutParams.WRAP_CONTENT
-    );
-    //buttonParams.setMargins(0, 0, 0, 10);
-    //LinearLayout.LayoutParams buttonParams = new LinearLayout.LayoutParams(
-    //        LinearLayout.LayoutParams.MATCH_PARENT,
-    //        LinearLayout.LayoutParams.WRAP_CONTENT);
-    //buttonParams.setMargins(0, 0, 0, 10);
-
-    constraintLayout.addView(myButton);
-    //linearlayout.addView(myButton, buttonParams);
-
-
     return pantryFragment;
+  }
+
+  @Override
+  public void onViewCreated(View view, Bundle savedInstanceState) {
+    Fragment childFragment = new ItemFragment();
+    FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+    transaction.replace(R.id.child_fragment_container, childFragment).commit();
+  }
+
+
+  @Override
+  public void onAttach(Context context) {
+    super.onAttach(context);
+    if (context instanceof OnFragmentInteractionListener) {
+      mListener = (OnFragmentInteractionListener) context;
+    } else {
+      throw new RuntimeException(context.toString()
+              + " must implement OnFragmentInteractionListener");
+    }
+  }
+
+  @Override
+  public void onDetach() {
+    super.onDetach();
+    mListener = null;
+  }
+
+  public interface OnFragmentInteractionListener {
+    // TODO: Update argument type and name
+    void messageFromParentFragment(Uri uri);
   }
 } // class
